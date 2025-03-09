b0df9490af2e0ceea059e0c40730cf239d05c15a
Zayne-Zafar <16zafara@gmail.com>
2025-03-09 18:45:16 +0000

    Made the playerController movement local to itself, added ground detection, and added a jump

--- Assets/Scripts/ZZ_PC_Movement.cs
+++ Assets/Scripts/ZZ_PC_Movement.cs
@@ -6,25 +6,27 @@ using UnityEngine;
 public class ZZ_PC_Movement : MonoBehaviour
 {
     private Rigidbody rb_PC;
+    private BoxCollider cl_groundDetector;
     [SerializeField] private float fl_movementSpeed = 5f;
     [SerializeField] private float fl_jumpSpeed = 5f;
     [SerializeField] private float fl_slowdownSpeed = 5f;
     [SerializeField] private float fl_maxGroundedSpeed = 15f;
     [SerializeField] private float fl_stopSpeed = 3f;
     [SerializeField] private bool bl_canMoveMidair = true;
-    private bool bl_grounded = true;
+    [SerializeField] private bool bl_grounded = true;
     private Vector3 v3_movementDirection = new Vector3(0, 0, 0);
 
     // Start is called before the first frame update
     void Start()
     {
         rb_PC = GetComponent<Rigidbody>();
+        cl_groundDetector = GetComponent<BoxCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        takeInput();
+        takeInput(bl_grounded);
     }
 
     void FixedUpdate()
@@ -42,33 +44,59 @@ public class ZZ_PC_Movement : MonoBehaviour
             }
         }
 
-        //Reduce speed if grounded and not moving
-        //if (bl_grounded && v3_movementDirection == new Vector3(0, 0, 0))
-        //{
-        //    slowDown();
-        //}
+        //Reduce speed if grounded
+        if (bl_grounded)
+        {
+            slowDown();
+        }
 
-        slowDown();
+        limitTopSpeed();
+    }
 
-        limitSpeed();
+    private void OnTriggerEnter(Collider other)
+    {
+        bl_grounded = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        bl_grounded = false;
     }
 
     //---------------------------------
 
-    //sets v3_movementDirection to normalised inputs
-    private void takeInput()
+    //sets v3_movementDirection to normalised, local inputs
+    private void takeInput(bool touchingGround)
     {
-        v3_movementDirection = new Vector3(
-            Input.GetAxis("Horizontal"), 
-            0, 
-            Input.GetAxis("Vertical")
-            ).normalized;
+        Vector3 v3_relativeMovementDirection;
+        
+        if(touchingGround)
+        {//if you're touching ground, then you can jump
+            v3_relativeMovementDirection = transform.InverseTransformDirection(new Vector3(
+                Input.GetAxis("Horizontal"),
+                Convert.ToSingle(Input.GetButton("Jump")),
+                Input.GetAxis("Vertical")
+                ).normalized);
+        }
+        else
+        {//otherwise you cant jump
+            v3_relativeMovementDirection = transform.InverseTransformDirection(new Vector3(
+                Input.GetAxis("Horizontal"),
+                0,
+                Input.GetAxis("Vertical")
+                ).normalized);
+        }
+
+        v3_movementDirection = v3_relativeMovementDirection;
     }
 
     //adds v3_movementDirection to velocity of rb_PC
     private void move()
     {
-        rb_PC.velocity += v3_movementDirection * fl_movementSpeed;
+        rb_PC.velocity += new Vector3(
+            v3_movementDirection.x * fl_movementSpeed, 
+            v3_movementDirection.y * fl_jumpSpeed, 
+            v3_movementDirection.z * fl_movementSpeed);
     }
 
     //gradualy reduces rb_PC.velocity to 0
@@ -106,7 +134,7 @@ public class ZZ_PC_Movement : MonoBehaviour
     }
 
     //limits horizontal rb_PC.velocity to v3_maxGroundSpeed
-    private void limitSpeed()
+    private void limitTopSpeed()
     {
         if (rb_PC.velocity.x > fl_maxGroundedSpeed)
         {
