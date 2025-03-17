27ccdf17a342bb79335b766883a6bdd9ac5c30a0
SohanThupakula <sohanthupakula0128@gmail.com>
2025-03-17 09:58:01 +0000

    Basic ice power, unfinished

---
+++ Assets/Scripts/ST_IcePlatform.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class ST_IcePlatform : MonoBehaviour
+{
+    [Header("Placement Parameters")]
+    [SerializeField] private GameObject placeableObjectPrefab;
+    [SerializeField] private GameObject previewObjectPrefab;
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private LayerMask placementSurfaceLayerMask;
+
+    [Header("Preview Material")]
+    [SerializeField] private Material previewMaterial;
+    [SerializeField] private Color validColor;
+    [SerializeField] private Color invalidColor;
+
+    [Header("Raycast Parameters")]
+    [SerializeField] private float objectDistanceFromPlayer;
+    [SerializeField] private float raycastStartVerticalOffset;
+    [SerializeField] private float raycastDistance;
+    [SerializeField] private float scrollSensitivity = 1f;
+
+
+    private GameObject _previewObject = null;
+    private Vector3 _currentPlacementPosition = Vector3.zero;
+    private bool _inPlacementMode = false;
+    private bool _validPreviewState = false;
+
+    private void Update()
+    {
+        UpdateInput();
+
+        if (_inPlacementMode)
+        {
+            UpdateCurrentPlacementPosition();
+
+            if (CanPlaceObject())
+                SetValidPreviewState();
+            else
+                SetInvalidPreviewState();
+        }
+    }
+
+    private void UpdateCurrentPlacementPosition()
+    {
+        Vector3 cameraForward = new Vector3(mainCamera.transform.forward.x, 0f, mainCamera.transform.forward.z);
+        cameraForward.Normalize();
+
+        Vector3 startPos = mainCamera.transform.position + (cameraForward * objectDistanceFromPlayer);
+        startPos.y += raycastStartVerticalOffset;
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(startPos, Vector3.down, out hitInfo, raycastDistance, placementSurfaceLayerMask))
+        {
+            _currentPlacementPosition = hitInfo.point;
+        }
+
+        Quaternion rotation = Quaternion.Euler(0f, mainCamera.transform.eulerAngles.y, 0f);
+        _previewObject.transform.position = _currentPlacementPosition;
+        _previewObject.transform.rotation = rotation;
+    }
+
+    private void UpdateInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            EnterPlacementMode();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ExitPlacementMode();
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            PlaceObject();
+        }
+
+        // Handle scroll wheel input
+        float scrollInput = Input.GetAxis("Mouse ScrollWheel");
+        if (scrollInput != 0f)
+        {
+            objectDistanceFromPlayer += scrollInput * scrollSensitivity;
+            objectDistanceFromPlayer = Mathf.Clamp(objectDistanceFromPlayer, 1f, 10f); // Adjust the min and max values as needed
+        } 
+    }
+
+    private void SetValidPreviewState()
+    {
+        previewMaterial.color = validColor;
+        _validPreviewState = true;
+    }
+
+    private void SetInvalidPreviewState()
+    {
+        previewMaterial.color = invalidColor;
+        _validPreviewState = false;
+    }
+
+    private bool CanPlaceObject()
+    {
+        if (_previewObject == null)
+            return false;
+
+        return _previewObject.GetComponentInChildren<ST_PlatformPreview>().IsValid;
+    }
+
+    private void PlaceObject()
+    {
+        if (!_inPlacementMode || !_validPreviewState)
+            return;
+
+        Quaternion rotation = Quaternion.Euler(0f, mainCamera.transform.eulerAngles.y, 0f);
+        Instantiate(placeableObjectPrefab, _currentPlacementPosition, rotation, transform);
+
+        ExitPlacementMode();
+    }
+
+    private void EnterPlacementMode()
+    {
+        if (_inPlacementMode)
+            return;
+
+        Quaternion rotation = Quaternion.Euler(0f, mainCamera.transform.eulerAngles.y, 0f);
+        _previewObject = Instantiate(previewObjectPrefab, _currentPlacementPosition, rotation, transform);
+        _inPlacementMode = true;
+    }
+
+    private void ExitPlacementMode()
+    {
+        Destroy(_previewObject);
+        _previewObject = null;
+        _inPlacementMode = false;
+    }
+}
---
+++ Assets/Scripts/ZZ_PC_Movement.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZZ_PC_Movement : MonoBehaviour
+{
+    private Rigidbody rb_PC;
+    private BoxCollider cl_groundDetector;
+    [SerializeField] private float fl_movementSpeed = 5f;
+    [SerializeField] private float fl_jumpSpeed = 5f;
+    [SerializeField] private float fl_slowdownSpeed = 5f;
+    [SerializeField] private float fl_maxGroundedSpeed = 15f;
+    [SerializeField] private float fl_stopSpeed = 3f;
+    [SerializeField] private bool bl_canMoveMidair = true;
+    [SerializeField] private bool bl_grounded = true;
+    private Vector3 v3_movementDirection = new Vector3(0, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb_PC = GetComponent<Rigidbody>();
+        cl_groundDetector = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        takeInput(bl_grounded);
+    }
+
+    void FixedUpdate()
+    {
+        //if you can move in mid-air, allow movement regardless of being grounded
+        if (bl_canMoveMidair)
+        {
+            move();
+        }
+        else
+        {
+            if (bl_grounded)
+            {
+                move();
+            }
+        }
+
+        //Reduce speed if grounded
+        if (bl_grounded)
+        {
+            slowDown();
+        }
+
+        limitTopSpeed();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        bl_grounded = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        bl_grounded = false;
+    }
+
+    //---------------------------------
+
+    //sets v3_movementDirection to normalised inputs
+    private void takeInput(bool touchingGround)
+    {
+        if (touchingGround)
+        {//if you're touching ground, then you can jump
+            v3_movementDirection = new Vector3(
+                Input.GetAxis("Horizontal"),
+                Convert.ToSingle(Input.GetButton("Jump")),
+                Input.GetAxis("Vertical")
+                ).normalized;
+        }
+        else
+        {//otherwise you cant jump
+            v3_movementDirection = new Vector3(
+                Input.GetAxis("Horizontal"),
+                0,
+                Input.GetAxis("Vertical")
+                ).normalized;
+        }
+    }
+
+    //adds v3_movementDirection to velocity of rb_PC depending on which way rb_PC is facing
+    private void move()
+    {
+        rb_PC.velocity += (transform.right * v3_movementDirection.x) * fl_movementSpeed;
+        rb_PC.velocity += (transform.up * v3_movementDirection.y) * fl_jumpSpeed;
+        rb_PC.velocity += (transform.forward * v3_movementDirection.z) * fl_movementSpeed;
+    }
+
+    //gradualy reduces rb_PC.velocity to 0
+    private void slowDown()
+    {
+        //decrease if above stop speed
+        if (rb_PC.velocity.x > fl_stopSpeed)
+        {
+            rb_PC.velocity -= new Vector3(fl_slowdownSpeed, 0, 0);
+        }
+        if (rb_PC.velocity.z > fl_stopSpeed)
+        {
+            rb_PC.velocity -= new Vector3(0, 0, fl_slowdownSpeed);
+        }
+
+        //increase if below negative stop speed
+        if (rb_PC.velocity.x < -fl_stopSpeed)
+        {
+            rb_PC.velocity += new Vector3(fl_slowdownSpeed, 0, 0);
+        }
+        if (rb_PC.velocity.z < -fl_stopSpeed)
+        {
+            rb_PC.velocity += new Vector3(0, 0, fl_slowdownSpeed);
+        }
+
+        //set to 0 if within positive and negative stop speed
+        if (rb_PC.velocity.x > -fl_slowdownSpeed && rb_PC.velocity.x < fl_slowdownSpeed)
+        {
+            rb_PC.velocity = new Vector3(0, rb_PC.velocity.y, rb_PC.velocity.z);
+        }
+        if (rb_PC.velocity.z > -fl_slowdownSpeed && rb_PC.velocity.z < fl_slowdownSpeed)
+        {
+            rb_PC.velocity = new Vector3(rb_PC.velocity.x, rb_PC.velocity.y, 0);
+        }
+    }
+
+    //limits horizontal rb_PC.velocity to v3_maxGroundSpeed
+    private void limitTopSpeed()
+    {
+        if (rb_PC.velocity.x > fl_maxGroundedSpeed)
+        {
+            rb_PC.velocity = new Vector3(fl_maxGroundedSpeed, rb_PC.velocity.y, rb_PC.velocity.z);
+        }
+        if (rb_PC.velocity.z > fl_maxGroundedSpeed)
+        {
+            rb_PC.velocity = new Vector3(rb_PC.velocity.x, rb_PC.velocity.y, fl_maxGroundedSpeed);
+        }
+
+
+        if (rb_PC.velocity.x < -fl_maxGroundedSpeed)
+        {
+            rb_PC.velocity = new Vector3(-fl_maxGroundedSpeed, rb_PC.velocity.y, rb_PC.velocity.z);
+        }
+        if (rb_PC.velocity.z < -fl_maxGroundedSpeed)
+        {
+            rb_PC.velocity = new Vector3(rb_PC.velocity.x, rb_PC.velocity.y, -fl_maxGroundedSpeed);
+        }
+    }
+}
